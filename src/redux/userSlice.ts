b1fab2cce0f82d548917737e4a3afb1edcbc1555
DatoGari <167792283+DatoGari@users.noreply.github.com>
2025-07-24 21:13:40 +0400

    Update userSlice.ts

--- src/redux/userSlice.ts
+++ src/redux/userSlice.ts
@@ -1,1 +1,29 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface UserState {
+  user: Record<string, any> | null;
+}
+
+const initialState: UserState = {
+  user: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser(state, action: PayloadAction<Record<string, any>>) {
+      state.user = action.payload;
+    },
+    updateUserInfo(state, action: PayloadAction<Record<string, any>>) {
+      state.user = {
+        ...state.user,
+        ...action.payload,
+      };
+    },
+  },
+});
+
+export const { setUser, updateUserInfo } = userSlice.actions;
+export default userSlice.reducer;
 
